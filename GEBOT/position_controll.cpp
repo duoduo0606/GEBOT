d322358891e4a718cf5e2189c00d49120658630a
duoduo0606 <56083200+duoduo0606@users.noreply.github.com>
2020-10-07 21:09:22 +0800

    v0.2

--- GEBOT/position_controll.cpp
+++ GEBOT/position_controll.cpp
@@ -8,53 +8,16 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include "position_controll.h"
+
+using namespace std;
 
 
-//------------------------------------------------------------
-//资料---------------------------------------------------------
-//------------------------------------------------------------
-/*
-int len;
-cin>>len;
-vector<int> array(len);//声明变长数组
-for(int i=0;i<len;i++)
-{
-array[i]=i;
-cout<<array[i]<<"\t";
-*/
-
-
-//------------------------------------------------------------
-//类定义-------------------------------------------------------
-//------------------------------------------------------------
-class Gebot_Gait                    //步态参数设置 函数功能设置
-{
-public:
-
-    //定义结构体-----------------------------------------------
-    struct Gait_para
-    {
-        float step_length;				//步距
-        float lateral_displacement;		//侧移
-        float step_hight;               //步高
-        float yaw_angle;				//偏航角
-    };
-
-    //定义参数-------------------------------------------------
-    float time_int;                 //时间间隔
-    float time_step;                //一次抬腿的时间
-    int resolution_ratio;
-    struct Gait_Para gait_para_state[],gaitPara;
-
-    //赋初值---------------------------------------------------
-    time_int = 0.1;
-    time_step = 1;
-
-    struct Gait_para gait_para_state[] =
-            {
+Gait_para gait_para_state[] =
+        {
                 //{step_length, lateral_displacement, step_hight, yaw_angle}
                 {0,0,0,0}, // STOP
-                {0,0,0,0}, // FORWARD
+                {60,0,0,0}, // FORWARD
                 {0,0,0,0}, // BACK
                 {0,0,0,0}, // LEFT
                 {0,0,0,0}, // RIGHT
@@ -62,104 +25,115 @@ public:
                 {0,0,0,0}, // RIGHT_TURN
                 {0,0,0,0}, // RESET
 
-            };
+        };
 
-    //计算-----------------------------------------------------
-    resolution_ratio = time_step/time_int;
+//计算-----------------------------------------------------
+resolution_ratio = time_step/time_int;
 
-    //步态规划 顺序间隔------------------------------------------
-    void gait_planning()
+//步态规划 顺序间隔------------------------------------------
+void gait_planning(state)
+{
+
+    //三角步态
+    vector<vector<float>> joint_theta_pack[][];
+
+    gaitPara = gait_para_state[FORWARD];
+
+    joint_theta_pack[0] = lifting_foot_trajectory_gene(gaitPara);
+    joint_theta_pack[1] = supporting_foot_trajectory_gene(gaitPara);
+    joint_theta_pack[2] = supporting_foot_trajectory_gene(gaitPara);
+    joint_theta_pack[3] = supporting_foot_trajectory_gene(gaitPara);
+
+}
+
+vector<float> Gebot_Gait::lifting_foot_trajectory_gene(struct Gait_para para_temp)
+{
+    //定义变量
+    float foot_px[],foot_py[],foot_pz[];
+    float diff_px,diff_py,diff_pz;
+    float old_px,old_py,old_pz;
+    int res,i;
+    vector<float> theta;
+
+    //赋初值
+    res = resolution_ratio;
+    foot_px[0] = 0;
+    foot_py[0] = 0;
+    foot_pz[0] = 0;
+
+    //设定轨迹曲线，离散存储，调用逆运动学函数
+    for (i = 0; i < res+1; i++)
     {
-        //FORWARD
-        //三角步态
-        RF = lifting_foot_trajectory_gene(gaitPara)
+        old_px = foot_px[i];
+        old_py = foot_py[i];
+        old_pz = foot_pz[i];
 
+        foot_px[i] = i*para_temp.step_length/res;
+        foot_py[i] = i*para_temp.lateral_displacement/res;
+        foot_pz[i] = -2*para_temp.step_hight/powf(para_temp.step_length,2)*foot_px[i]*(foot_px[i]-para_temp.step_length);
 
+        diff_px = foot_px[i] - old_px;
+        diff_py = foot_py[i] - old_py;
+        diff_pz = foot_pz[i] - old_pz;
 
-
-
+        theta = DH_inv_kinematics(diff_px,diff_py,diff_pz,para_temp.yaw_angle);
+        return theta;
     }
+}
 
-    //足端轨迹--------------------------------------------------
-    //说明 para_temp包含了状态关系，对应状态的步态参数
-    void lifting_foot_trajectory_gene(struct Gait_para para_temp)
+Gebot_Gait::supporting_foot_trajectory_gene(struct Gait_para para_temp)//para_temp与移动脚一致，具体移动参数在函数中处理
+{
+    //定义变量
+    float foot_px[],foot_py[],foot_pz[];
+    float diff_px,diff_py,diff_pz;
+    float old_px,old_py,old_pz;
+    int res,i;
+    vector<float> theta;
+
+    //赋初值
+    res = resolution_ratio;
+    foot_px[0] = 0;
+    foot_py[0] = 0;
+    foot_pz[0] = 0;
+
+    //设定轨迹曲线，离散存储，调用逆运动学函数
+    for (i = 0; i < res+1; i++)
     {
-        //定义变量
-        float foot_px[],foot_py[],foot_pz[];
-        float diff_px,diff_py,diff_pz;
-        float old_px,old_py,old_pz;
-        int res,i;
+        old_px = foot_px[i];
+        old_py = foot_py[i];
+        old_pz = foot_pz[i];
 
-        //赋初值
-        res = resolution_ratio;
-        foot_px[0] = 0;
-        foot_py[0] = 0;
-        foot_pz[0] = 0;
+        foot_px[i] = -1/3*i*para_temp.step_length/res;
+        foot_py[i] = -1/3*i*para_temp.lateral_displacement/res;
 
-        //设定轨迹曲线，离散存储，调用逆运动学函数
-        for (i = 0; i < res+1; i++)
-        {
-            old_px = foot_px[i];
-            old_py = foot_py[i];
-            old_pz = foot_pz[i];
+        diff_px = foot_px[i] - old_px;
+        diff_py = foot_py[i] - old_py;
+        diff_pz = foot_pz[i] - old_pz;
 
-            foot_px[i] = i*para_temp.step_length/res;
-            foot_py[i] = i*para_temp.lateral_displacement/res;
-            foot_pz[i] = -2*para_temp.step_hight/powf(para_temp.step_length,2)*foot_px[i]*(foot_px[i]-para_temp.step_length);
+        theta = DH_inv_kinematics(diff_px,diff_py,diff_pz);
 
-            diff_px = foot_px[i] - old_px;
-            diff_py = foot_py[i] - old_py;
-            diff_pz = foot_pz[i] - old_pz;
-
-            DH_inv_kinematics(diff_px,diff_py,diff_pz,para_temp.yaw_angle)
-        }
+        return theta;
     }
+}
 
-    void supporting_foot_trajectory_gene(struct Gait_para para_temp)//para_temp与移动脚一致，具体移动参数在函数中处理
-    {
-        //定义变量
-        float foot_px[],foot_py[],foot_pz[];
-        float diff_px,diff_py,diff_pz;
-        float old_px,old_py,old_pz;
-        int res,i;
+//逆运动学----------------------------------------------------
+Gebot_Gait::DH_inv_kinematics(float px, float py, float pz)
+{
+    //定义变量
+    vector<float> theta;
+    float l1,l2,l3;
+    float m;
 
-        //赋初值
-        res = resolution_ratio;
-        foot_px[0] = 0;
-        foot_py[0] = 0;
-        foot_pz[0] = 0;
+    //theta.push_back(atan2(-px,py) - atan2(sqrt(px*px+py*py-l3*l3),l3));
 
-        //设定轨迹曲线，离散存储，调用逆运动学函数
-        for (i = 0; i < res+1; i++)
-        {
-            old_px = foot_px[i];
-            old_py = foot_py[i];
-            old_pz = foot_pz[i];
+    theta[0] = atan2(-px,py) - atan2(sqrt(px*px+py*py-l3*l3),l3);
+    m = px*cos(theta[0]) + py*sin(theta[0]);
+    theta[2] = acos((m*m+pz*pz-l1*l1-l2*l2)/(2*l1*l2));
+    theta[1] = atan2(pz*(l1+l2*cos(theta[2]))-l2*sin(theta[2]),m*(l1+l2*cos(theta[2]))+pz*l2*sin(theta[2]));
 
-            foot_px[i] = -1/3*i*para_temp.step_length/res;
-            foot_py[i] = -1/3*i*para_temp.lateral_displacement/res;
+    return theta;
+}
 
-            diff_px = foot_px[i] - old_px;
-            diff_py = foot_py[i] - old_py;
-            diff_pz = foot_pz[i] - old_pz;
-
-            DH_inv_kinematics(diff_px,diff_py,diff_pz,para_temp.yaw_angle)
-        }
-    }
-
-    //逆运动学----------------------------------------------------
-    void DH_inv_kinematics(float px, float py, float pz, float yaw)
-    {
-        //
-    }
-
-
-
-
-
-    /*void toSTOP()
-    void toFORWARD()
-    void toLEFT()*/
 
 };
 //------------------------------------------------------------
--- GEBOT/position_controll.h
+++ GEBOT/position_controll.h
@@ -5,7 +5,7 @@
 #ifndef POSITION_CONTROLL_POSITION_CONTROLL_H
 #define POSITION_CONTROLL_POSITION_CONTROLL_H
 
-
+using namespace std;
 
 enum States
         {
@@ -19,7 +19,58 @@ enum States
             RESET = 7,
         };
 
+//------------------------------------------------------------
+//类定义-------------------------------------------------------
+//------------------------------------------------------------
+class Gebot_Gait                    //步态参数设置 函数功能设置
+{
+public:
 
+    //定义结构体-----------------------------------------------
+    struct Gait_para
+    {
+        float step_length;				//步距
+        float lateral_displacement;		//侧移
+        float step_hight;               //步高
+        float yaw_angle;				//偏航角
+    };
+
+    //定义参数-------------------------------------------------
+    float time_int = 0.1;                 //时间间隔
+    float time_step = 1;                //一次抬腿的时间
+    int resolution_ratio;
+    Gait_para gaitPara;
+    int state;
+
+    //赋初值---------------------------------------------------
+    //time_int = 0.1;
+    //time_step = 1;
+
+    /*
+    struct Gait_para gait_para_state[] =
+            {
+                    //{step_length, lateral_displacement, step_hight, yaw_angle}
+                    {0,0,0,0}, // STOP
+                    {60,0,0,0}, // FORWARD
+                    {0,0,0,0}, // BACK
+                    {0,0,0,0}, // LEFT
+                    {0,0,0,0}, // RIGHT
+                    {0,0,0,0}, // LEFT_TURN
+                    {0,0,0,0}, // RIGHT_TURN
+                    {0,0,0,0}, // RESET
+
+            };
+    */
+
+    //计算-----------------------------------------------------
+    //resolution_ratio = time_step/time_int;
+
+    //步态规划 顺序间隔------------------------------------------
+    void gait_planning();
+
+    vector<float> lifting_foot_trajectory_gene(struct Gait_para para_temp);
+    vector<float> supporting_foot_trajectory_gene(struct Gait_para para_temp);
+    vector<float> DH_inv_kinematics(float px, float py, float pz);
 
 
 #endif //POSITION_CONTROLL_POSITION_CONTROLL_H
